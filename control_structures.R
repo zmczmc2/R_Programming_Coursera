f8a8fa1c9eb001fae772063d529083892ac8d6c3
Zack Chambers <zmchambers89@gmail.com>
2022-06-03 07:28:15 -0700

    added some next code

--- control_structures.R
+++ control_structures.R
@@ -42,4 +42,48 @@ for (i in seq_len(nrow(matty))) {
 } 
 
 
+# TESTING SKIPPING ITERATIONS WITH NEXT
+
+for (i in 1:20) {
+  
+  if (i > 10) {
+    
+    next
+  }
+  
+  print(i)
+  
+  
+}
+
+
+char_vec <- c('a', 'b', 'c', 'd')
+
+
+for (char in char_vec) {
+  
+  if (char == 'a' | char == 'b') {
+    
+    next
+  }
+  
+  print(char)
+  
+  
+}
+
+
+logi <- c(T,F,F,F,T,T,FALSE)
+
+for (value in logi) {
+  
+  if (value != F) {
+    
+    next
+  }
+  print(value)
+  
+}
+
+
 
